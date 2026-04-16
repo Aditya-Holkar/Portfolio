f946d319b06d0fb7e21247fb47a671aa9860a05a
Aditya-Holkar <adiholkar555@gmail.com>
2026-04-16 17:47:07 +0530

    Colour change

--- src/Component/Button.jsx
+++ src/Component/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Button = (props) => {
   return (
-    <button className="btn rounded-4xl max-md:w-full bg-[#0d0d0d] border-solid border-[#fcfcfc] hover:bg-[#8a8a93]">
+    <button className="btn rounded-4xl max-md:w-full bg-[#0d0d0d] border-solid border-[#fcfcfc] hover:bg-[#8a8a93] text-[#fcfcfc]">
       {props.children}
     </button>
   );
